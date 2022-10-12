1a5c382ea2d88f92dcfcae9ab2939a23cab4fd7f
juggernautsei <sherwingaddis@gmail.com>
2022-10-12 13:32:45 -0400

    Hiding element in the intake form until selected.

--- sites/default/LBF/LBFIntake.plugin.php
+++ sites/default/LBF/LBFIntake.plugin.php
@@ -7,5 +7,11 @@ function LBFIntake_javascript_onload()
        living.style.display = 'none';
        const threequater = document.getElementById('form_three_qtr_intake');
        threequater.style.display = 'none';
+       const halfway = document.getElementById('form_name_halfway_intake');
+       halfway.style.display = 'none';
+       const treatment = document.getElementById('form_Name_of_Residential_Treatment_P');
+       treatment.style.display = 'none';
+       const shelter = document.getElementById('form_Name_of_Shelter');
+       shelter.style.display = 'none';
     ";
 }
