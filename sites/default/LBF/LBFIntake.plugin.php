2d1402c8b3492c4a447742457b18580e119595a3
juggernautsei <sherwingaddis@gmail.com>
2022-10-12 13:17:16 -0400

    Hiding element in the intake form until selected.

--- sites/default/LBF/LBFIntake.plugin.php
+++ sites/default/LBF/LBFIntake.plugin.php
@@ -3,9 +3,10 @@
 function LBFIntake_javascript_onload()
 {
     echo "
-       const livinglabel = document.getElementById('label_id_apartment_no_intake');
-       livinglabel.display = 'none';
+      
        const living = document.getElementById('form_apartment_no_intake');
        living.style.display = 'none';
+       const threequater = document.getElementById('form_three_qtr_intake');
+       threequater.style.display = 'none';
     ";
 }
